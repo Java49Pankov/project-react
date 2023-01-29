a7f400f1176669e7cf1e3a0ba063ce4c996eed87
Evgeny Pankov <volvovnl123@gmail.com>
2023-01-29 16:30:48 +0200

    solution hw#36

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { getByDisplayValue } from '@testing-library/react';
 import React from 'react';
-import { Input } from './components/Input';
-
-
 
 function App() {
-  const properties: React.CSSProperties = {
-    display: "flex",
-    flexWrap: "wrap"
-  }
-  const [colors, setColors] = React.useState<string[]>([])
-  function creatingDivs(value: string): string {
-    const colors: string[] = value.split("#"); //red#green => ["red", "green"]
-    setColors(colors.slice());
-    return '';
+  
 
-  }
-  function getDivs(colors: string[]): JSX.Element[] {
-    return colors.map(color => <div style={{width: "5vw", height: "5vw",
-     backgroundColor: color }}></div>)
-  }
-  return <section style = {{display: "flex", flexDirection:"column"}}>
-    <Input placeHolder={'enter colors separated by #'} inputProcess={creatingDivs}></Input>
-    <section style={properties}>
-        {getDivs(colors)}
-    </section>
-  </section>
+  return <div >
+  </div>
+
 }
 
 export default App;
