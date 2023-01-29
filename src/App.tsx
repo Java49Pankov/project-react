8ce285ffe843b24970d78dff0091693ecdc2a4e8
Evgeny Pankov <volvovnl123@gmail.com>
2023-01-29 16:22:16 +0200

    teacher solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,31 @@
+import { getByDisplayValue } from '@testing-library/react';
 import React from 'react';
-import { Timer } from './components/Timer';
 import { Input } from './components/Input';
 
 
 
 function App() {
-  // const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
-  // const flexRow: React.CSSProperties = {
-  //   display: "flex", flexDirection: "row",
-  //   justifyContent: "space-around", width: "50vw", marginTop: "4vh"
-  // }
   const properties: React.CSSProperties = {
     display: "flex",
     flexWrap: "wrap"
   }
+  const [colors, setColors] = React.useState<string[]>([])
+  function creatingDivs(value: string): string {
+    const colors: string[] = value.split("#"); //red#green => ["red", "green"]
+    setColors(colors.slice());
+    return '';
 
-  const [cityCountry, setCityCountry] = React.useState<string[]>([]);
-
-  function creatingDivs(valueCityCountry: string): string {
-    valueCityCountry = valueCityCountry.toLowerCase();
-    valueCityCountry = valueCityCountry.charAt(0).toUpperCase() + cityCountry.slice(1)
-    const cityOrCountry: string[] = valueCityCountry.split("#");
-    setCityCountry(cityOrCountry.slice());
-    return ''
   }
-
-  function getDivs(cityCountry: string[]): JSX.Element[] {
-    return cityCountry.map(value => <div><Timer cityOrCountry={value}></Timer></div>)
+  function getDivs(colors: string[]): JSX.Element[] {
+    return colors.map(color => <div style={{width: "5vw", height: "5vw",
+     backgroundColor: color }}></div>)
   }
-
-  return <section style={{ display: "flex", flexDirection: "column" }}>
-    <Input placeHolder={'enter cities or countries separated by #'} inputProcess={creatingDivs} ></Input>
+  return <section style = {{display: "flex", flexDirection:"column"}}>
+    <Input placeHolder={'enter colors separated by #'} inputProcess={creatingDivs}></Input>
     <section style={properties}>
-      {getDivs(cityCountry)}
+        {getDivs(colors)}
     </section>
-  </section >
-
+  </section>
 }
 
 export default App;
