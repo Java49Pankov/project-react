ff749cf294942a6d12e9eba9c03c95511991f962
Evgeny Pankov <volvovnl123@gmail.com>
2023-02-01 18:18:25 +0200

    HW #37 life game solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Life } from './components/Life';
+import lifeConfig from './config/life-game.json';
+
 
 function App() {
-  
-
-  return <div >
+  return <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }} >
+    <Life dimensions={lifeConfig.dimensions} ticInterval={lifeConfig.ticInterval}></Life>
   </div>
 
 }
--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,25 +1,27 @@
 export function getRandomNumber(min: number, max: number,
-     isMinInclusive: boolean = true, isMaxInclusive: boolean = false) {
-        if (!isMinInclusive) {
-            min++;
-        }
-        if (isMaxInclusive) {
-            max++;
-        }
-        if (min == max) {
-            throw "min may not be equaled to max";
-        }
-        if (min > max) {
-            [min, max] = [max, min];
-        }
-        return min + Math.trunc(Math.random() * (max - min));
-        
+    isMinInclusive: boolean = true, isMaxInclusive: boolean = false) {
+    if (!isMinInclusive) {
+        min++;
+    }
+    if (isMaxInclusive) {
+        max++;
+    }
+    if (min == max) {
+        throw "min may not be equaled to max";
+    }
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+    return min + Math.trunc(Math.random() * (max - min));
+
 }
-export function getElement(array: any[]): any {
+
+export function getElement<T>(array: T[]): T {
     return array[getRandomNumber(0, array.length)];
 }
-export function getRandomMatrix(rows: number, columns: number, min: number,
-     max: number): number[][] {
+
+export function getRandomMatrix(rows: number, columns: number,
+    min: number, max: number): number[][] {
     const resMatrix: number[][] = [[]];
     for (let i = 0; i < rows; i++) {
         resMatrix[i] = [];
@@ -29,6 +31,7 @@ export function getRandomMatrix(rows: number, columns: number, min: number,
     }
     return resMatrix;
 }
+
 export function getRandomDate(minYear: number, maxYear: number): Date {
     const year = getRandomNumber(minYear, maxYear);
     const month = getRandomNumber(0, 12);
