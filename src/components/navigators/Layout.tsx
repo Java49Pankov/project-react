1f6877718e19d14b1c10430e5f701de56bee998e
Evgeny Pankov <volvovnl123@gmail.com>
2023-02-09 16:42:46 +0200

    updated CW40 active button

--- src/components/navigators/Layout.tsx
+++ src/components/navigators/Layout.tsx
@@ -1,20 +1,27 @@
 import { NavLink, Outlet } from 'react-router-dom';
 import './navigators.css'
 export const Layout: React.FC = () => {
+    function activeLink(isActive: boolean): React.CSSProperties | undefined {
+        let res: React.CSSProperties = {};
+        if (isActive) {
+            res = { backgroundColor: "blue", color: "white" }
+        }
+        return res;
+    }
     return <div>
         <nav>
             <ul className="navigator-list">
                 <li className="navigator-item">
-                    <NavLink to="/">Home</NavLink>
+                    <NavLink style={({isActive})=> activeLink(isActive) } to="/">Home</NavLink>
                 </li>
                 <li className="navigator-item">
-                    <NavLink to="/customers">Customers</NavLink>
+                    <NavLink style={({isActive})=> activeLink(isActive) }to="/customers">Customers</NavLink>
                 </li>
                 <li className="navigator-item">
-                    <NavLink to="/orders">Orders</NavLink>
+                    <NavLink style={({isActive})=> activeLink(isActive) }to="/orders">Orders</NavLink>
                 </li>
                 <li className="navigator-item">
-                    <NavLink to="/products">Products</NavLink>
+                    <NavLink style={({isActive})=> activeLink(isActive) }to="/products">Products</NavLink>
                 </li>
             </ul>
         </nav>
