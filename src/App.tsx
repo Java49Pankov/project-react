4a268f7fe1cd3ed56b1df6e42d1c6c86c45cbe66
Evgeny Pankov <volvovnl123@gmail.com>
2023-02-11 10:30:18 +0200

    HW#40 Navigator component , instead of Layout and Products

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css'
-import { Layout } from './components/navigators/Layout';
-import { Products } from './components/navigators/Products';
+import { Navigator } from './components/navigators/Navigator';
 import { BreadProducts } from './components/pages/BreadProducts';
 import { Customers } from './components/pages/Customers';
 import { DairyProducts } from './components/pages/DairyProducts';
 import { Home } from './components/pages/Home';
 import { Orders } from './components/pages/Orders';
+import { layoutConfig } from './models/layout-config';
+import { productsConfig } from './models/products-config';
 
 function App() {
 
   return <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Navigator navigatorProps={layoutConfig} />}>
         <Route index element={<Home />} />
         <Route path='customers' element={<Customers />} />
         <Route path='orders' element={<Orders />} />
-        <Route path='products' element={<Products />}>
+        <Route path='products' element={<Navigator navigatorProps={productsConfig} />}>
           <Route path='dairy' element={<DairyProducts />} />
           <Route path='bread' element={<BreadProducts />} />
         </Route>
---
+++ src/models/NavigatorProps.ts
@@ -0,0 +1,8 @@
+
+export type NavigatorProps = {
+    cssClassName: string;
+    navigatorRouter: {
+        routingPath: string,
+        label: string
+    }[];
+}
