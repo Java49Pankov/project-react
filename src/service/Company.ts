04ed1bc5f93d3540fa79af71725060a1162191d7
Evgeny Pankov <volvovnl123@gmail.com>
2023-02-22 18:05:37 +0200

    HW #43 solution DataGrid actions and authorization

--- src/service/Company.ts
+++ src/service/Company.ts
@@ -3,27 +3,25 @@ import { Employee } from "../model/Employee";
 export class Company {
     private employees: Employee[] = [];
     addEmployee(empl: Employee): void {
-        this.employees.push(empl)
+        this.employees.push(empl);
     }
-
     updateEmployee(empl: Employee): void {
-        const emplUpdated = this.getEmployee(empl.id);
-        if (emplUpdated != null) {
-            emplUpdated.department = empl.department;
-            emplUpdated.salary = empl.salary;
+        const index = this.employees.findIndex(e => e.id == empl.id);
+        
+        if (index >= 0 ) {
+           
+           this.employees[index] = empl;
         }
+        
     }
-
     getEmployee(id: number): Employee | null {
-        const index: number = this.employees.findIndex(empl => empl.id === id)
+        const index: number = this.employees.findIndex(e => e.id === id);
         return index < 0 ? null : this.employees[index];
     }
-
     removeEmployee(id: number): void {
-        const index: number = this.employees.findIndex(empl => empl.id === id);
-        index >= 0 && this.employees.splice(index, 1);
+        const index: number = this.employees.findIndex(e => e.id === id);
+        index >= 0 && this.employees.splice(index, 1) ;
     }
-
     getAllEmployees(): Employee[] {
         return this.employees.slice();
     }
