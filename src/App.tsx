50506ffb7214855304d83a4b0c1c68f43309314c
Evgeny Pankov <volvovnl123@gmail.com>
2023-02-26 17:29:45 +0200

    HW #44 Edit Add from table, generation

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,39 @@ import { RouteType } from './model/RouteType';
 import { useSelector } from 'react-redux';
 import { Login } from './components/pages/Login';
 import { Logout } from './components/pages/Logout';
+import { Generation } from './components/pages/Generation';
 
 
 function App() {
     const [routes, setRoutes] = useState<RouteType[]>([]);
-    const authUser: string = useSelector<any, string>(state => state.auth.authenticated);
-    useEffect(() => {
+    const authUser:string = useSelector<any,string>(state=>state.auth.authenticated );
+    useEffect(()=> {
         function getRoutes(): RouteType[] {
-            const logoutRoute: RouteType | undefined = layoutConfig.routes.find(route => route.path.includes("logout"));
+            const logoutRoute: RouteType |undefined = layoutConfig.routes
+            .find(r => r.path.includes('logout'))
             logoutRoute!.label = authUser;
             return layoutConfig.routes.filter(r => (!authUser && !r.flAuth) ||
-                (authUser.includes('admin') && r.flAdmin) ||
-                (!!authUser && r.flAuth && !r.flAdmin))
+            (authUser.includes('admin') && r.flAdmin) ||
+            (authUser && r.flAuth && !r.flAdmin))
         }
         setRoutes(getRoutes());
     }, [authUser])
-    return <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Navigator routes={routes} />}>
-                <Route index element={<Employees />} />
-                <Route path='add' element={<AddEmployee />} />
-                <Route path='statistics/age' element={<AgeStatistics />} />
-                <Route path='statistics/salary' element={<SalaryStatistics />} />
-                <Route path='login' element={<Login />} />
-                <Route path='logout' element={<Logout />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
+  return <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Navigator 
+           routes={routes}  />}>
+              <Route index element={<Employees/>}/>
+              <Route path='add' element={<AddEmployee/>}/>
+              <Route path='statistics/age' element={<AgeStatistics/>}/>
+              <Route path='statistics/salary' element={<SalaryStatistics/>}/>
+              <Route path='login' element={<Login/>}/>
+              <Route path='logout' element={<Logout/>}/>
+              <Route path='generation' element={<Generation/>}/>
+              
+          </Route>
+              
+      </Routes>
+  </BrowserRouter>
 
 }
 export default App;
