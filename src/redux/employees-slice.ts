0718981f970f7061f4c6ba392f2aa636b3d10c19
Evgeny Pankov <volvovnl123@gmail.com>
2023-03-20 19:02:24 +0200

    CW #49 Firebase Authentication Authorization

--- src/redux/employees-slice.ts
+++ src/redux/employees-slice.ts
@@ -1,28 +1,91 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { Employee } from '../model/Employee';
-import { Company } from '../service/Company';
-const company = new Company();
-const initialState: {employees: Employee[]} = {
-    employees: company.getAllEmployees()
+import { CompanyFirebase } from '../service/CompanyFirebase';
+import { codeActions } from './codeSlice';
+const company = new CompanyFirebase();
+const initialState: { employees: Employee[] } = {
+    employees: []
 }
 const employeesSlice = createSlice({
     initialState,
     name: "company",
     reducers: {
-        addEmployee: (state, data) =>
-        {
-            company.addEmployee(data.payload);
-            state.employees = company.getAllEmployees();
-        },
-        removeEmployee: (state, data) => {
-            company.removeEmployee(data.payload);
-            state.employees = company.getAllEmployees();
-        },
-        updateEmployee: (state, data) => {
-            company.updateEmployee(data.payload);
-            state.employees = company.getAllEmployees();
+        setEmployees: (state, data) => {
+            state.employees = data.payload;
         }
     }
 })
+
 export const employeesReducer = employeesSlice.reducer;
-export const employeesActions = employeesSlice.actions;
+const actions = employeesSlice.actions;
+export const employeesActions: any = {
+    addEmployee: (empl: Employee) => {
+        return async (dispatch: any) => {
+            try {
+                await company.addEmployee(empl);
+                dispatch(employeesActions.getEmployees());
+                dispatch(codeActions.setCode("OK"));
+            } catch (error: any) {
+                dispatch(codeActions.setCode("Authorization error"));
+            }
+
+        }
+    },
+    updateEmployee: (empl: Employee) => {
+        return async (dispatch: any) => {
+            try {
+                await company.updateEmployee(empl);
+                dispatch(employeesActions.getEmployees());
+                dispatch(codeActions.setCode("OK"));
+            } catch (error: any) {
+                dispatch(codeActions.setCode("Authorization error"));
+            }
+
+        }
+    },
+    removeEmployee: (id: number) => {
+        return async (dispatch: any) => {
+            try {
+                await company.removeEmployee(id);
+                dispatch(employeesActions.getEmployees());
+                dispatch(codeActions.setCode("OK"));
+            } catch (error: any) {
+                dispatch(codeActions.setCode("Authorization error"));
+            }
+
+
+        }
+    },
+    getEmployees: () => {
+        return async (dispatch: any) => {
+            try {
+                const employees = await company.getAllEmployees();
+                dispatch(actions.setEmployees(employees));
+                dispatch(codeActions.setCode("OK"));
+            } catch (error: any) {
+                dispatch(codeActions.setCode("Unknown Error"));
+            }
+
+        }
+    },
+    addBulkEmployees: (employeesAr: Employee[]) => {
+        return async (dispatch: any) => {
+            for(let i = 0; i < employeesAr.length; i++) {
+                try{
+                    await company.addEmployee(employeesAr[i]);
+                    dispatch(codeActions.setCode("OK"));
+                }catch(error: any) {
+                    dispatch(codeActions.setCode("Authorization error"));
+                    return;
+                } 
+            }
+               
+            
+           
+            const employees = await company.getAllEmployees();
+            dispatch(actions.setEmployees(employees));
+        }
+    }
+
+
+}
--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,15 +1,15 @@
-import { LoginData } from '../model/LoginData'
+import {LoginData} from '../model/LoginData'
 export class AuthService {
     private users: LoginData[] = [
-        { username: "user@gmail.com", password: "user1234" },
-        { username: "admin@gmail.com", password: "admin1234" }
+        {username: "user@gmail.com", password: "user1234"},
+        {username: "admin@gmail.com", password: "admin1234"}
     ];
-
-    login(loginData: LoginData): void {
-        const userIndex = this.users.findIndex(user => user.username === loginData.username && user.password === loginData.password);
-        if (userIndex === -1) {
-            throw "User not found"
-        }
+    login(loginData: LoginData) {
+     
+      const user = this.users.find(u => loginData.username === u.username);
+      if (!user || user.password !== loginData.password) {
+        throw 'Wrong credentials';
+      }
     }
 
 }
